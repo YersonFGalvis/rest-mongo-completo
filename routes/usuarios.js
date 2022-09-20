f65e9dd9efc8f447b8a8a9c64dd895036cf759a5
Yertrix <yersongalvis03@gmail.com>
2022-09-19 19:03:14 -0500

    roles con bd

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-
+const Role = require('../models/role');
 const {
   usuariosGet,
   usuariosDelete,
@@ -22,10 +22,14 @@ router.post("/",[
   check('nombre', 'El nombre es obligatorio').not().isEmpty(),
   check('password', 'El password debe ser de mas de 6 letras').isLength({ min: 6}),
   check('correo', 'El correo no es valido').isEmail(),
-  check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+  check('rol').custom( async (rol = '')=>{
+    const existeRol = await Role.findOne({rol});
+    if (!existeRol){
+      throw new Error(`El rol ${rol} no esta registrado en la BD`)
+    }
+  }),
   validarCampos
-],
- usuariosPost);
+],usuariosPost);
 
 router.delete("/", usuariosDelete);
 
